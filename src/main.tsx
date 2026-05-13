69293816972fc1b197a67b466932c06579211f0c
bizzauto <bizzauto.solution@gmail.com>
2026-05-14 01:48:55 +0530

    fix: white screen - remove circular dependency in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import AppWrapper from "./AppWrapper";
-import apiClient from "./lib/api";
 
-(window as any).__API_CLIENT__ = apiClient;
-(window as any).__BUILD_HASH__ = 'a4b8344';
+console.log('App starting...');
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
